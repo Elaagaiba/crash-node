411920344c60f730541bc2a0a9b6dee86e93f14a
elaagaiba abdelaadim <elaagaiba33kech@gmail.com>
2023-09-12 17:00:15 +0100

    page 404 !!! and installing EJS
    Signed-off-by: elaagaiba abdelaadim <elaagaiba33kech@gmail.com>

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ app.listen(3000);
 
 app.get('/', (req,res) => {
     res.sendFile('./views/index.html' , {root: __dirname})
-})
+});
 
 app.get('/about', (req,res) => {
     res.sendFile('./views/about.html' , {root: __dirname})
-})
+});
 
 //redirect 
 app.get('/about_us', (req ,res) => {
     res.redirect('/about')
-})
+});
 
-//404 !!!
+//404 !!! you should write it in the buttom of the direction pages 
+app.use((req , res) => {
+    res.status(404).sendFile('./views/404.html' ,{root: __dirname})
+})
