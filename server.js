34fc7ba8107905be27b7dd8a2262830782c8ec7a
elaagaiba abdelaadim <elaagaiba33kech@gmail.com>
2023-09-12 12:35:25 +0100

    add lodash in dependencies
    Signed-off-by: elaagaiba abdelaadim <elaagaiba33kech@gmail.com>

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const _ = require('lodash');
 const server = http.createServer((req,res) => {
 
     //lodash
-
+    const num = _.random(0, 20);
+    console.log(num);
+    
+    const greet = _.once(() => {
+      console.log('hello');
+    });
+    greet();
+    greet();
 
     //set header content type
 
