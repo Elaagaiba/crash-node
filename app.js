eea4a0ac5c363f134ebd679abebc02ba7f63e1a2
elaagaiba abdelaadim <elaagaiba33kech@gmail.com>
2023-09-13 01:45:42 +0100

    add the create page
    Signed-off-by: elaagaiba abdelaadim <elaagaiba33kech@gmail.com>

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const express = require("express");
 //express app 
 const app = express();
 
+//register view engie
+app.set('view engine' ,'ejs')
+
 //listen for request 
 app.listen(3000);
 
 app.get('/', (req,res) => {
-    res.sendFile('./views/index.html' , {root: __dirname})
+    res.render('index' )
 });
 
 app.get('/about', (req,res) => {
-    res.sendFile('./views/about.html' , {root: __dirname})
+    res.render('about')
 });
 
+app.get('/blogs/create',(req, res) => {
+    res.render('create')
+})
 //redirect 
-app.get('/about_us', (req ,res) => {
-    res.redirect('/about')
-});
+// app.get('/about_us', (req ,res) => {
+//     res.redirect('/about')
+// });
+
 
 //404 !!! you should write it in the buttom of the direction pages 
 app.use((req , res) => {
-    res.status(404).sendFile('./views/404.html' ,{root: __dirname})
+    res.status(404).render('404')
 })
